7452898e5341455f77f83b12f6aad5d40dc5664f
NS <2300030644@kluniversity.in>
2025-08-02 09:56:41 +0530

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import './App.css';
+import './style.css';
 
 function App() {
   return (
     <div className="container">
       <h1>Welcome to My React Project - S205</h1>
+      <p>CI/CD and Cloud DevOps</p>
       <p>This is a simple homepage deployed on GitHub Pages using Vite.</p>
-      <p>CI/CD CLOUD DEVOPS</p>
     </div>
   );
 }
